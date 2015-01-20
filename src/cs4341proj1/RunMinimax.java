3df2a01b9d56753bba4eed0ea0e05d1110f6d019
Andrew Roskuski <aroskuski@gmail.com>
2015-01-19 19:50:33 -0500

    Implemented some threaded code

--- src/cs4341proj1/RunMinimax.java
+++ src/cs4341proj1/RunMinimax.java
@@ -5,7 +5,11 @@ public class RunMinimax implements Runnable {
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		
+		GameBoard board = GameBoard.getInstance();
+		for(int i = 0; i < 5; i++){
+			board.calculatePly();
+			board.minimax();
+		}
 	}
 
 }
